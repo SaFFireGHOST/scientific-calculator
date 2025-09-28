49efdaba9209528f82ac6dd060be2c90a5e76fe3
SaFFireGHOST <Koushik.Mupparapu@iiitb.ac.in>
2025-09-28 10:59:18 +0530

    Updated test case in test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,7 +8,7 @@ def test_sqrt():
         sqrt(-1)
 
 def test_factorial():
-    assert factorial(5) == 120
+    assert factorial(6) == 720
     with pytest.raises(ValueError):
         factorial(-1)
 
